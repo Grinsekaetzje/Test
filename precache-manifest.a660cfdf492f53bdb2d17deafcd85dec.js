9cfc90c1037c1da3f86c96aeb2b19f32d63c89a8
Patrick O'Connor <poco0001@student.monash.edu>
2019-07-10 16:16:52 +1000

    Updates

---
+++ precache-manifest.a660cfdf492f53bdb2d17deafcd85dec.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fc6b855a8aed16fe013884178cd028",
+    "url": "/Test/index.html"
+  },
+  {
+    "revision": "097011ebea4335908388",
+    "url": "/Test/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "3c29ce278911c3b71dc2",
+    "url": "/Test/static/css/main.e65c658d.chunk.css"
+  },
+  {
+    "revision": "097011ebea4335908388",
+    "url": "/Test/static/js/2.16eb1851.chunk.js"
+  },
+  {
+    "revision": "3c29ce278911c3b71dc2",
+    "url": "/Test/static/js/main.e7fa7985.chunk.js"
+  },
+  {
+    "revision": "b1f86fac8fee76c864e6",
+    "url": "/Test/static/js/runtime~main.a19dbc3c.js"
+  },
+  {
+    "revision": "0f66d4afa79eb6396f9c2951b2f8975d",
+    "url": "/Test/static/media/Home.0f66d4af.jpg"
+  },
+  {
+    "revision": "9e5385bcae45bed5450897615ebde41e",
+    "url": "/Test/static/media/img1.9e5385bc.jpg"
+  },
+  {
+    "revision": "a471b7c14fabcb0e7859197ad949c01a",
+    "url": "/Test/static/media/img5.a471b7c1.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Test/static/media/logo.5d5d9eef.svg"
+  }
+]);
